41d52d7f078d218ec612fc7caf3035462e7b5dd5
ThetHtar-Vivian <127220089+ThetHtar-Vivian@users.noreply.github.com>
2025-09-19 23:06:31 +0630

    updating the system

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -11,6 +11,7 @@ public class App
     {
         // Connect to MongoDB on local system - we're using port 27000
         // Connect to MongoDB
+        // Connect to MongoDB
         MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
